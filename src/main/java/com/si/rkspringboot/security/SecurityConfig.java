12c83bd07e355e5df0259be86eea688c466488e7
Andrea-LaRoss <andrea.199020@gmail.com>
2022-06-29 17:40:19 +0200

    Security

--- src/main/java/com/si/rkspringboot/security/SecurityConfig.java
+++ src/main/java/com/si/rkspringboot/security/SecurityConfig.java
@@ -20,13 +20,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserRepository userRepository;
 
+    public static final String[] ADMIN_URL_MATCHER = {
+            "/api/cars/remove/**",
+            "/api/cars/add",
+
+            "/api/users/add",
+            "/api/users/remove/**",
+            "/api/users",
+    };
+
+    public static final String[] COMMON_URL_MATCHER = {
+            "/api/cars",
+            "/api/reservations",
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(new AuthorizationFilter(authenticationManager(), this.userRepository), UsernamePasswordAuthenticationFilter.class).authorizeRequests()
-                .antMatchers("/api/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers(ADMIN_URL_MATCHER).hasAnyRole("ADMIN")
+                .antMatchers(COMMON_URL_MATCHER).hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated();
     }
 
